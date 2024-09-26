17a85006e1be57b1da2f20c0f1ce7987bb608dc6
wangkai.1997 <wangkka1@163.com>
2024-09-26 19:23:53 +0800

    fix: change lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
-import antfu from '@antfu/eslint-config';
+import antfu from "@antfu/eslint-config";
 
 export default antfu({
-  formatters: true,
+  formatters: {
+    css: false,
+    html: true,
+    markdown: true,
+  },
+  typescript: true,
   react: true,
   stylistic: {
+    indent: 2,
+    quotes: "double",
     semi: true,
+  },
+  rules: {
+    "no-alert": 0,
+    "no-console": 0,
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/method-signature-style": 0,
   },
 });
